731d3a9bc12741b2f81d209ec429c3e50e3882c5
wolfstons <paczi.balazs@gmail.com>
2026-04-23 13:42:31 +0200

    v3

--- index.js
+++ index.js
@@ -3,31 +3,25 @@ import Termekek from "./Termekek.js";
 
 const taroloElem = document.querySelector(".tartalom");
 const termekElem = document.querySelector("#termekek");
-const felhElem = document.querySelector("#felhasznalok");
+const felhElem = document.querySelector("#felh");
 
 let lista = [];
-termekElem.addEventListener("click",()=>{
+termekElem.addEventListener("click", () => {
+  const services = new Services();
+  services.getadat("https://fakestoreapi.com/products", termekMegjelenitese);
 
+  function termekMegjelenitese(data) {
+    lista = data;
+    new Termekek(lista, taroloElem);
+  }
+});
 
-const services = new Services();
-services.getadat("https://fakestoreapi.com/products", termekMegjelenitese);
+felhElem.addEventListener("click", () => {
+  let usersList = [];
+  services.getadat("https://fakestoreapi.com/users", felhMegjelenitese);
 
-function termekMegjelenitese(data) {
-  lista = data;
-  new Termekek(lista, taroloElem);
-
-  
-}
-})
-
-felhElem.addEventListener("click",()=>{
-
-let usersList = [];
-services.getadat("https://fakestoreapi.com/users", felhMegjelenitese);
-
-
-function felhMegjelenitese(felhAdatok) {
-  usersList = felhAdatok;
-  new Termekek(usersList, taroloElem);
-}
-})
+  function felhMegjelenitese(felhAdatok) {
+    usersList = felhAdatok;
+    new Termekek(usersList, taroloElem);
+  }
+});

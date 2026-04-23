60d6726d170ad2e42969eb4ec13d1450fe29932c
wolfstons <paczi.balazs@gmail.com>
2026-04-23 13:47:20 +0200

    v5

--- index.js
+++ index.js
@@ -4,24 +4,23 @@ import Termekek from "./Termekek.js";
 const taroloElem = document.querySelector(".tartalom");
 const termekElem = document.querySelector("#termekek");
 const felhElem = document.querySelector("#felh");
-
+console.log(felhElem);
 let lista = [];
+let usersList = [];
+const services = new Services();
 termekElem.addEventListener("click", () => {
-  const services = new Services();
   services.getadat("https://fakestoreapi.com/products", termekMegjelenitese);
-
-  function termekMegjelenitese(data) {
-    lista = data;
-    new Termekek(lista, taroloElem);
-  }
 });
+
+function termekMegjelenitese(data) {
+  lista = data;
+  new Termekek(lista, taroloElem);
+}
 
 felhElem.addEventListener("click", () => {
-  let usersList = [];
   services.getadat("https://fakestoreapi.com/users", felhMegjelenitese);
-
-  function felhMegjelenitese(felhAdatok) {
-    usersList = felhAdatok;
-    new Termekek(usersList, taroloElem);
-  }
 });
+function felhMegjelenitese(felhAdatok) {
+  usersList = felhAdatok;
+  new Termekek(usersList, taroloElem);
+}
